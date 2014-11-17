c8d7ee547fd40eb524ae0feaf7ac6c16faed147f
eric <erdisa1415daw2@gmail.com>
2014-11-17 23:19:42 +0100

    “Versió 0.3 del projecte prjava01”

--- src/projecte_beta/Aplicacio.java
+++ src/projecte_beta/Aplicacio.java
@@ -1,26 +1,18 @@
 
-package projecte_beta;
-
 /**
  *
  * @author eric
+ * @Date 20141117
  */
-//import java.io.BufferedReader;
-import java.io.Console;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+
+package projecte_beta;
+
+
+import java.io.Console;
 import java.util.Scanner;
 
 
-/**
- *
- * @author eric
- */
  
 public class Aplicacio {
 
@@ -28,27 +20,41 @@ public class Aplicacio {
     
     public static void main(String[] args) {
         
-        Console cnsl = System.console();
-         
+         Console cnsl = null;         
          Scanner sc = new Scanner (System.in);
+         Cotxe c[] = new Cotxe[5];
+         Matricula m[] = new Matricula[5];
          
-        
+         menu();
                 
-                Cotxe c[] = new Cotxe[2];
+        try {
+            
+             cnsl = System.console();
+             Scanner in = new Scanner(System.in);
+             
+             
+             if (cnsl != null) {
+                 
+              int opcio = in.nextInt(); 
+              
+              while ( opcio != 0){
                 
-                for(int i = 0; i< c.length ; i++) {
-                    
-                    
-                    
+                   if ( opcio == 1 ){
+                       
+               for(int i = 0; i< c.length ; i++) {
+                                         
+                     
                     System.out.println("Marca\n");
                     String marca = sc.nextLine();
                     
                     System.out.println("Model\n");
                     String model = sc.nextLine();
                     
-                    System.out.println("Matricula\n");
-                    String matricula = sc.nextLine();
-                    
+                    System.out.println("Lletres Matricula\n");
+                    String lletras_matri = sc.nextLine();
+                                                            
+                    System.out.println("Numero Matricula\n");
+                    int num_matri = sc.nextInt();
                     
                     System.out.println("Any compra\n");
                     int data = sc.nextInt();
@@ -56,27 +62,90 @@ public class Aplicacio {
                     System.out.println("Plaçes \n");
                     int places = sc.nextInt();
                     
+                    System.out.println("Consum als 100\n");
+                    float consum = sc.nextInt();
                     
+                    System.out.println("Extres\n");
+                    String extres = cnsl.readLine();
+                                        
                     sc.nextLine();
                     
-                    c[i] = new Cotxe (marca, model, matricula, data, places);
+                    c[i] = new Cotxe (marca, model,extres, data, places,consum);
                     
+                    m[i] = new Matricula (lletras_matri,num_matri);
                     
-                   // System.out.println(c[i].getMarca()+" "+c[i].getModel()+" "+c[i].getNumPlaçes());
+                    System.out.println("Vehicle enregistrat correctament\n");
                     
                                 }
-                
+                     
+                   menu2();
+                   opcio = in.nextInt();
+                   
+                   }
+                   
+                if (opcio == 2){
                 System.out.println("\nLlistat de Cotxes creats: \n");
                 
-               System.out.println( "Marca: "+c[0].getMarca()+"\nModel: "+c[0].getModel()+
-                       "\nAny: "+c[0].getData()+"\nMatricula: "+c[0].getMatricula()+
-                       "\nNumero plaçes:  "+c[0].getNumPlaçes());
+                System.out.println( "Marca: "+c[0].getMarca()+"\nModel: "+c[0].getModel()+
+                        "\nMatricula "+m[0].getLletras_matri()+m[0].getNum_matri()
+                        +"\nAny: "+c[0].getData()+"\nNumero plaçes:  "+c[0].getNumPlaçes()+
+                        "\nConsum: "+c[0].getConsum()+"\nExtres:"+c[0].getExtres()+"\n\n");
+                
+                System.out.println( "Marca: "+c[1].getMarca()+"\nModel: "+c[1].getModel()+
+                        "\nMatricula "+m[1].getLletras_matri()+m[1].getNum_matri()
+                        +"\nAny: "+c[1].getData()+"\nNumero plaçes:  "+c[1].getNumPlaçes()+
+                        "\nConsum: "+c[1].getConsum()+"\nExtres:"+c[1].getExtres()+"\n\n");
                
-               System.out.println( "Marca: "+c[1].getMarca()+"\nModel: "+c[1].getModel()+
-                       "\nAny: "+c[1].getData()+"\nMatricula: "+c[1].getMatricula()+
-                       "\nNumero plaçes:  "+c[1].getNumPlaçes());
-               
-               System.out.println("\nNumero Cotxes creats a la BBDD :\n" + Cotxe.getNombreCotxes());
+                System.out.println( "Marca: "+c[2].getMarca()+"\nModel: "+c[2].getModel()+
+                        "\nMatricula "+m[2].getLletras_matri()+m[2].getNum_matri()
+                        +"\nAny: "+c[2].getData()+"\nNumero plaçes:  "+c[2].getNumPlaçes()+
+                        "\nConsum: "+c[2].getConsum()+"\nExtres:"+c[2].getExtres()+"\n\n");
+                
+                System.out.println( "Marca: "+c[3].getMarca()+"\nModel: "+c[3].getModel()+
+                        "\nMatricula "+m[3].getLletras_matri()+m[3].getNum_matri()
+                        +"\nAny: "+c[3].getData()+"\nNumero plaçes:  "+c[3].getNumPlaçes()+
+                        "\nConsum: "+c[3].getConsum()+"\nExtres:"+c[3].getExtres()+"\n\n");
+                
+               System.out.println( "Marca: "+c[4].getMarca()+"\nModel: "+c[4].getModel()+
+                        "\nMatricula "+m[4].getLletras_matri()+m[4].getNum_matri()
+                        +"\nAny: "+c[4].getData()+"\nNumero plaçes:  "+c[4].getNumPlaçes()+
+                        "\nConsum: "+c[4].getConsum()+"\nExtres:"+c[4].getExtres()+"\n\n");
+                
+                System.out.println("\nNumero Cotxes creats a la BBDD :\n" + Cotxe.getNombreCotxes());
+                }
+                
+                break;
+                                    
+                }
+             }
+        }
+        
+    catch(Exception e){
+                  
+        System.out.println("Cal introduir un valor correcte a la llista");
+        
+      }
     }
+            /**
+             * Mètode que ens mostra un menú personalitzat amb 3 opcions
+             * diferents. 
+             */
+            public static void menu(){
+           
+           System.out.println("[1] Nou Ingres de vehicles");
+           System.out.println("[2] Llistat de Cotxes");
+           System.out.println("[0] Sortir");
+           
+     }
+            /**
+             * Mètode que presenta un menú personalitzat després de 
+             * l'entrada dels 5 vehicles
+             */
             
+            public static void menu2(){
+           
+           System.out.println("[2] Llistat de Cotxes");
+           System.out.println("[0] Sortir");
+           
+     }
     }
